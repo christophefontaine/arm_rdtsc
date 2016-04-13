08db4c590e7b407f3c26d559dcf3a1c68611a87f
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-04-14 07:50:17 +0900

    Display per cpu counter

--- test/rdtsc_test.c
+++ test/rdtsc_test.c
@@ -1,17 +1,33 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <sched.h>
+#include <sys/sysinfo.h>
 
-int main(int argc, char ** argv) {
+
+int worker(void* arg) {
+    int core_id = *(int*)arg;
     unsigned int tsc, tsc2, i;
     unsigned long start, end;
-    asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r" (tsc));
+
     asm volatile("mrrc p15, 0, %Q0, %R0, c9" : "=r" (start));
     for (i=0; i < 1000; i++) {
     };
-    asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r" (tsc2));
     asm volatile("mrrc p15, 0, %Q0, %R0, c9" : "=r" (end));
+    printf("[%d] end: %12u  delta64: %6u\n", core_id, end, end-start);
+    return 0;
+}
 
-    printf("delta = %u\n", tsc2-tsc);
-    printf("delta64 = %u\n", end-start);
+
+int main(int argc, char ** argv) {
+    int cpu;
+    for(cpu = 0; cpu < get_nprocs() ; cpu++ )
+    {
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(cpu, &cpuset);
+        sched_setaffinity(pthread_self(), sizeof(cpu_set_t), &cpuset);
+        worker(&cpu);
+    }
 
     return 0;
 }
