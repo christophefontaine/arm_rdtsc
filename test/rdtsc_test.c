1bd3eb1c0a2f4fa9cfae385f9b6a9d5ddccf07d6
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-04-13 11:42:26 +0900

    Retreive 64 bit value for tsc

--- test/rdtsc_test.c
+++ test/rdtsc_test.c
@@ -2,10 +2,16 @@
 
 int main(int argc, char ** argv) {
     unsigned int tsc, tsc2, i;
+    unsigned long start, end;
     asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r" (tsc));
+    asm volatile("mrrc p15, 0, %Q0, %R0, c9" : "=r" (start));
     for (i=0; i < 1000; i++) {
     };
     asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r" (tsc2));
-    printf("delta = %d\n", tsc2-tsc);
+    asm volatile("mrrc p15, 0, %Q0, %R0, c9" : "=r" (end));
+
+    printf("delta = %u\n", tsc2-tsc);
+    printf("delta64 = %u\n", end-start);
+
     return 0;
 }
